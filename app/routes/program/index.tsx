170c79c6bfdedea81255b29e5dfc078662921fee
William Hamparsomian <william.hamparsomian@svt.se>
2022-08-31 21:37:01 +0200

    Add Heteropia 2 program

--- app/routes/program/index.tsx
+++ app/routes/program/index.tsx
@@ -3,7 +3,7 @@ import { Link, useLoaderData } from "@remix-run/react";
 
 // ------- Lägg till alla nya poster här:
 import * as post1 from "./oxbron.mdx";
-// import * as post2 from "./samtal.mdx";
+import * as post2 from "./kolhuset.mdx";
 // import * as post2 from "./why-capitalism-works.mdx";
 // import * as post3 from "./capitalism-is-gret.mdx";
 // import * as post4 from "./en-till-utstallning.mdx";
@@ -13,7 +13,7 @@ import parseFrontMatter from "front-matter";
 // -------- Och här:
 const allPosts = [
   getPostData(post1),
-  // getPostData(post2),
+  getPostData(post2),
   // getPostData(post3),
   // getPostData(post4),
 ];
