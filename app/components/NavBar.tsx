9486142a87a290ae14b98c1ca68080eec89dbc6c
William <william.hamparsomian@svt.se>
2023-06-06 17:15:40 +0200

    Remove HNK link in NavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -8,20 +8,6 @@ export const NavBar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   return (
     <nav className="flex flex-wrap items-center justify-between p-6">
-      <div className="mr-6 flex flex-shrink-0 items-center">
-        <Link className="flex" to="/aktuellt">
-          {/* <svg
-            className="mr-2 h-8 w-8 fill-current"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg> */}
-          <span className="text-xl font-semibold tracking-tight">HNK</span>
-        </Link>
-      </div>
       <div className="block lg:hidden">
         <button
           className="hover:text-blue flex items-center rounded border border-gray-400 px-3 py-2 hover:border-white"
